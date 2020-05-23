dc6016987c9fc7d654ed9b125c4054a5edd5ce36
Dorival <dorival.querino@gmail.com>
2020-05-23 10:48:31 -0300

    Api gateway - commit inicial

---
+++ 04-padroes_microservices/padroes_para_composicao_de_microservicos/api_gateway/jornada-gateway/src/test/java/br/com/jornada_gateway/jornadagateway/JornadaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jornada_gateway.jornadagateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JornadaGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

c55d6bd6b2ba73eb2b706005c49bf5b34cac9ede
Dorival <dorival.querino@gmail.com>
2020-10-22 22:16:13 -0300

    Business Capabilities - adicionado monolito inicial

---
+++ 03-modelagem_e_design/decomposicao/business_capabilities/alimentacao-system-inicial/src/main/java/br/com/jornada/ms/clientes/ClienteController.java
@@ -0,0 +1,16 @@
+package br.com.jornada.ms.clientes;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+	
+
+	@GetMapping()
+	public String getClientes() {
+		return "Retornando cliente";
+	}
+}
